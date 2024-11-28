7df5214783532aa157f6c16d9804c724190dad7a
Jennifer Kim <jenkim@mit.edu>
2024-11-27 23:07:52 -0600

    temp sizing for clothing images

--- client/components/Clothing/ClothingImageComponent.vue
+++ client/components/Clothing/ClothingImageComponent.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import { useUserStore } from "@/stores/user";
-import { storeToRefs } from "pinia";
-import { fetchy } from "../../utils/fetchy";
-import { onMounted, ref } from "vue";
-
 const props = defineProps(["imgUrl"]);
 </script>
 
 <template>
-    <div>
-        <img :src="props.imgUrl" alt="Clothing Image" />
-    </div>
+  <div>
+    <img :src="props.imgUrl" alt="Clothing Image" />
+  </div>
 </template>
+
+<style scoped>
+img {
+  width: 100px;
+  height: auto;
+}
+</style>
