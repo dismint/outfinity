9a60889358af51efa9770b9ae9a77b1de2722dff
Tiana Jiang <116186910+tianajiang@users.noreply.github.com>
2024-12-01 14:20:44 -0500

    challenging winners logic

--- server/concepts/challenging.ts
+++ server/concepts/challenging.ts
@@ -11,6 +11,7 @@ export interface ChallengeDoc extends BaseDoc {
   submissions: Map<ObjectId, ObjectId>; // Map<outfit, user>
   active: boolean;
   requiredItem: ObjectId | null;
+  winners: ObjectId[];
 }
 
 /**
@@ -27,7 +28,7 @@ export default class ChallengingConcept {
   }
 
   async create(name: string, description: string, owner: ObjectId, closet: ObjectId, requiredItem: ObjectId | null) {
-    const _id = await this.challenges.createOne({ name, description, owner, closet, submissions: new Map(), active: true, requiredItem });
+    const _id = await this.challenges.createOne({ name, description, owner, closet, submissions: new Map(), active: true, requiredItem, winners: [] });
     return { msg: "Challenge successfully created!", challenge: await this.challenges.readOne({ _id }) };
   }
 
@@ -98,6 +99,56 @@ export default class ChallengingConcept {
       throw new NotFoundError(`Challenge ${_id} does not exist!`);
     }
     return challenge.submissions;
+  }
+
+  async addChallengeWinner(_id: ObjectId, winner: ObjectId) {
+    const challenge = await this.challenges.readOne({ _id });
+    if (!challenge) {
+      throw new NotFoundError(`Challenge ${_id} does not exist!`);
+    }
+    let winnerDidChallenge = false;
+    for (const user of challenge.submissions.values()) {
+      if (user.toString() === winner.toString()) {
+        winnerDidChallenge = true;
+      }
+    }
+    if (!winnerDidChallenge) {
+      throw new NotAllowedError(`User ${winner} did not participate in challenge ${_id}!`);
+    }
+    if (challenge.winners.length === 3) {
+      throw new NotAllowedError(`Challenge ${_id} already has 3 winners!`);
+    }
+    let winnerAlreadyPicked = false;
+    for (const w of challenge.winners) {
+      if (w.toString() === winner.toString()) {
+        winnerAlreadyPicked = true;
+      }
+    }
+    if (winnerAlreadyPicked) {
+      throw new NotAllowedError(`User ${winner} is already a winner of challenge ${_id}!`);
+    }
+    challenge.winners.push(winner);
+    await this.challenges.collection.updateOne({ _id }, { winners: challenge.winners });
+    return { msg: "Winner successfully added!" };
+  }
+
+  async removeChallengeWinner(_id: ObjectId, winner: ObjectId) {
+    const challenge = await this.challenges.readOne({ _id });
+    if (!challenge) {
+      throw new NotFoundError(`Challenge ${_id} does not exist!`);
+    }
+    let winnerIsWinner = false;
+    for (const w of challenge.winners) {
+      if (w.toString() === winner.toString()) {
+        winnerIsWinner = true;
+      }
+    }
+    if (!winnerIsWinner) {
+      throw new NotAllowedError(`User ${winner} is not a winner of challenge ${_id}!`);
+    }
+    challenge.winners = challenge.winners.filter((w) => w.toString() !== winner.toString());
+    await this.challenges.collection.updateOne({ _id }, { winners: challenge.winners });
+    return { msg: "Winner successfully removed!" };
   }
 }
 
