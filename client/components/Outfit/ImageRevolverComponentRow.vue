a76cdc7664b3e99b8ac249560a91274f9222b852
dismint <mintjjc@gmail.com>
2024-12-04 16:20:58 -0500

    oops

---
+++ client/components/Outfit/ImageRevolverComponentRow.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps, ref } from "vue";
+import ClothingImageComponent from "../Clothing/ClothingImageComponent.vue";
+
+const props = defineProps(["images", "oneimage", "type"]);
+const imageOrder = ref(props.images.slice());
+</script>
+
+<template>
+  <div class="outerRevolver">
+    <div v-if="props.images.length !== 0">
+      <div class="centered" v-if="props.oneimage">
+        <ClothingImageComponent :imgUrl="props.images" />
+      </div>
+      <div class="" v-else>
+        <ClothingImageComponent v-for="(image, index) in imageOrder" :key="index" :imgUrl="image" :style="{ zIndex: index, position: 'absolute', scale: 1 - index * 0.2 }" />
+      </div>
+    </div>
+    <p v-else class="noClothes">No {{ props.type }}</p>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  margin: 0;
+}
+
+.outerRevolver {
+  box-sizing: content-box;
+  height: 100%;
+  aspect-ratio: 1;
+  border: 0.4vmin solid var(--dark-green);
+  margin: 1%;
+  position: relative;
+}
+
+.noClothes {
+  height: 100%;
+  aspect-ratio: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Inter";
+  font-weight: 700;
+  font-size: 1vmin;
+  color: var(--dark-green);
+}
+</style>
---
+++ client/components/Outfit/OutfitImageComponentRow.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import ImageRevolverComponentRow from "@/components/Outfit/ImageRevolverComponentRow.vue";
+import { fetchy } from "@/utils/fetchy";
+import { defineProps, onBeforeMount, ref } from "vue";
+
+const props = defineProps(["outfit"]);
+const loaded = ref(false);
+const headImage = ref("");
+const topImages = ref<Array<string>>([]);
+const bottomImages = ref<Array<string>>([]);
+const onepieceImage = ref("");
+const shoeImage = ref("");
+
+const getImages = async () => {
+  for (const c of props.outfit.clothes) {
+    let clothingResult;
+    try {
+      clothingResult = await fetchy(`/api/clothes`, "GET", { query: { id: c }, alert: false });
+    } catch (_) {
+      return;
+    }
+    if (clothingResult.type === "hat") {
+      headImage.value = clothingResult.imgUrl;
+    } else if (clothingResult.type === "top") {
+      topImages.value.push(clothingResult.imgUrl);
+    } else if (clothingResult.type === "bottom") {
+      bottomImages.value.push(clothingResult.imgUrl);
+    } else if (clothingResult.type === "onepiece") {
+      onepieceImage.value = clothingResult.imgUrl;
+    } else if (clothingResult.type === "shoe") {
+      shoeImage.value = clothingResult.imgUrl;
+      console.log("shoeImage", shoeImage);
+    }
+  }
+};
+
+onBeforeMount(async () => {
+  console.log("props.outfit", props.outfit);
+  await getImages();
+  loaded.value = true;
+});
+</script>
+
+<template>
+  <div class="row oimageContainer">
+    <ImageRevolverComponentRow v-if="loaded && headImage" :imageLoaded="loaded" :images="headImage" :oneimage="true" :type="'hat'" />
+    <ImageRevolverComponentRow v-if="loaded && topImages" :imageLoaded="loaded" :images="topImages" :oneimage="false" :type="'top'" />
+    <ImageRevolverComponentRow v-if="loaded && bottomImages" :imageLoaded="loaded" :images="bottomImages" :oneimage="false" :type="'bottom'" />
+    <ImageRevolverComponentRow v-if="loaded && onepieceImage" :imageLoaded="loaded" :images="onepieceImage" :oneimage="true" :type="'onepiece'" />
+    <ImageRevolverComponentRow v-if="loaded && shoeImage" :imageLoaded="loaded" :images="shoeImage" :oneimage="true" :type="'shoe'" />
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+.oimageContainer {
+  height: 100%;
+}
+</style>
