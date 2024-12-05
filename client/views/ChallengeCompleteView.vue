1f35998bf71e83d484782bc1494032fb54cd7542
Jennifer Kim <jenkim@mit.edu>
2024-12-05 13:23:15 -0500

    🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉

--- client/views/ChallengeCompleteView.vue
+++ client/views/ChallengeCompleteView.vue
@@ -2,38 +2,37 @@
 import router from "@/router";
 
 const goToHome = () => {
-  router.push("/");
+  void router.push("/");
 };
-
 </script>
 
 <template>
-    <div class="challenge-complete">
-        <h1>Challenge Complete!</h1>
-        <p>Congratulations on completing the challenge!</p>
-        <button @click="goToHome">Go Home</button>
-    </div>
+  <div class="challenge-complete">
+    <h1>Challenge Complete!</h1>
+    <p>🎉Congratulations on completing the challenge!🎉</p>
+    <button @click="goToHome">Go Home</button>
+  </div>
 </template>
 
 <style scoped>
 .challenge-complete {
-    text-align: center;
-    margin-top: 50px;
+  text-align: center;
+  margin-top: 50px;
 }
 
 .challenge-complete h1 {
-    font-size: 2em;
-    margin-bottom: 20px;
+  font-size: 2em;
+  margin-bottom: 20px;
 }
 
 .challenge-complete p {
-    font-size: 1.2em;
-    margin-bottom: 30px;
+  font-size: 1.2em;
+  margin-bottom: 30px;
 }
 
 .challenge-complete button {
-    padding: 10px 20px;
-    font-size: 1em;
-    cursor: pointer;
+  padding: 10px 20px;
+  font-size: 1em;
+  cursor: pointer;
 }
 </style>
